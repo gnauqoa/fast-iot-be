8a74cd8f7dbdeb5c1fb7c5bec45dfd553f17c79a
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-04-24 14:41:01 +0700

    refactor: improve formatting in UpdateDeviceUserIdNullable migration

--- src/database/migrations/1745464805229-UpdateDeviceUserIdNullable.ts
+++ src/database/migrations/1745464805229-UpdateDeviceUserIdNullable.ts
@@ -1,19 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UpdateDeviceUserIdNullable1745464805229 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+export class UpdateDeviceUserIdNullable1745464805229
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "device"
             ALTER COLUMN "userId" DROP NOT NULL;
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "device"
             ALTER COLUMN "userId" SET NOT NULL;
         `);
-    }
-
+  }
 }
